9474db32d7b9d5949e7c7a9ca232a8e74d32e378
Brent Gauthier <cajundrummer@gmail.com>
2024-05-22 16:37:21 -0400

    Complete register function

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -1,22 +1,35 @@
 const db = require('../db')
+const {hash} = require('bcryptjs')
 
 exports.getUsers = async (req, res) => {
     try {
-        const {rows} = await db.query('SELECT * FROM users')
-        console.log(rows)
+        const {rows} = await db.query('SELECT user_id, email FROM users')
+
+        return res.status(200).json({
+            success: true,
+            users: rows
+        })
     } catch (err) {
         console.log(err.message)
     }
 }
 
 exports.register = async (req, res) => {
+    const {email, password} = req.body
     try {
-        // const {rows} = await db.query(
-        //     'INSERT INTO users VALUES($1, $2)'
-        // )
-        // console.log(rows)
-        console.log('validation test passed')
+        const hashedPassword = await hash(password, 10)
+        
+        await db.query('INSERT INTO users (email, password) VALUES ($1, $2)', [email, hashedPassword])
+
+        return res.status(201).json({
+            success: true,
+            message: 'User created successfully'
+        })
+
     } catch (err) {
         console.log(err.message)
+        return res.status(500).json({
+            error: error.message,
+        })
     }
 }
