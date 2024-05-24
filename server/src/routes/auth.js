7ae384c494efaf5c849f368595d7c2c31ecd9d3e
Brent Gauthier <cajundrummer@gmail.com>
2024-05-23 21:14:00 -0400

    Import middleware & set up routing to test protected routing

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,10 +1,12 @@
 const {Router} = require('express')
-const { getUsers, register, login } = require('../controllers/auth')
+const { getUsers, register, login, protected } = require('../controllers/auth')
 const {validationMiddleware} = require('../middlewares/validations-middleware')
+const {userAuth} = require('../middlewares/auth-middleware')
 const { registerValidation, loginValidation } = require('../validators/auth')
 const router = Router()
 
 router.get('/get-users', getUsers)
+router.get('/protected', userAuth, protected)
 router.post('/register', registerValidation, validationMiddleware, register)
 router.post('/login', loginValidation, validationMiddleware, login)
 
